6cd23b1846a74e08ba724a9b095c8aa21be85907
John Lilley <johnbot@caltech.edu>
2024-01-10 10:57:49 -0800

    cluster scripts from new cryodocs

--- cluster_script.sh
+++ cluster_script.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/env bash
+#### cryoSPARC cluster submission script template for SLURM
+## Available variables:
+## {{ run_cmd }}            - the complete command string to run the job
+## {{ num_cpu }}            - the number of CPUs needed
+## {{ num_gpu }}            - the number of GPUs needed. 
+##                            Note: the code will use this many GPUs starting from dev id 0
+##                                  the cluster scheduler or this script have the responsibility
+##                                  of setting CUDA_VISIBLE_DEVICES so that the job code ends up
+##                                  using the correct cluster-allocated GPUs.
+## {{ ram_gb }}             - the amount of RAM needed in GB
+## {{ job_dir_abs }}        - absolute path to the job directory
+## {{ project_dir_abs }}    - absolute path to the project dir
+## {{ job_log_path_abs }}   - absolute path to the log file for the job
+## {{ worker_bin_path }}    - absolute path to the cryosparc worker command
+## {{ run_args }}           - arguments to be passed to cryosparcw run
+## {{ project_uid }}        - uid of the project
+## {{ job_uid }}            - uid of the job
+## {{ job_creator }}        - name of the user that created the job (may contain spaces)
+## {{ cryosparc_username }} - cryosparc username of the user that created the job (usually an email)
+## {{ job_type }}           - CryoSPARC job type
+##
+## What follows is a simple SLURM script:
+
 #SBATCH --job-name cryosparc_{{ project_uid }}_{{ job_uid }}
 #SBATCH -N 1
 #SBATCH -n {{ num_cpu }}
+## Not sure if the following are needed any longer. testing required. johnbot
+## SBATCH --time=120:00:00
+## SBATCH --exclusive
 #SBATCH --gres=gpu:{{ num_gpu }}
-#SBATCH --mem={{ (ram_gb*1024)|int }}MB             
-#SBATCH --time=120:00:00
-#SBATCH --exclusive
-#SBATCH --output={{ job_dir_abs }}output.txt
-#SBATCH --error={{ job_dir_abs }}error.txt
-
-available_devs=""
-for devidx in $(seq 0 15);
-do
-if [[ -z $(nvidia-smi -i $devidx --query-compute-apps=pid --format=csv,noheader) ]] ; then
-if [[ -z "$available_devs" ]] ; then
-available_devs=$devidx
-else
-available_devs=$available_devs,$devidx
-fi
-fi
-done
-export CUDA_VISIBLE_DEVICES=$available_devs
+## We don't use partitions
+## SBATCH --partition=gpu
+#SBATCH --mem={{ (ram_gb*1024)|int }}MB
+#SBATCH --output={{ job_log_path_abs }}
+#SBATCH --error={{ job_log_path_abs }}
 
 {{ run_cmd }}
