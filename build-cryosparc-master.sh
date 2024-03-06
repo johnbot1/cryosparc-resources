383a0c99ab0468822fbe6ab50188fcc2018eaa57
John Lilley <johnbot@caltech.edu>
2024-03-06 12:35:40 -0800

    build cryo-master

--- build-cryosparc-master.sh
+++ build-cryosparc-master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-masterhostname=vislogin1.cm.cluster
+masterhostname=vislogin2.cm.cluster
 
 
 usage() { echo "Usage: $0 [-g <groupdir>] [-e <email>] [-l <license id>] -p <master port>]" 1>&2; exit 1; }
@@ -94,6 +94,15 @@ cryosparcm createuser --email $EMAIL --password $generated_password --username $
 #
 #
 
+# Copy the default cluster templates
+cp ./cluser_info.json /central/groups/$GROUPDIR/$USER/software/cryosparc
+cp ./cluster_script.sh /central/groups/$GROUPDIR/$USER/software/cryosparc
+
+sed -i '/"worker_bin_path"/d' /central/groups/$GROUPDIR/$USER/software/cryosparc/cluster_info.json
+sed -i '/"cache_path"/d' /central/groups/$GROUPDIR/$USER/software/cryosparc/cluster_info.json
+
+sed -i '3i\ \ \ \ "worker_bin_path" : "/central/groups/'"$GROUPDIR"'/'"$USER"'/software/cryosparc/cryosparc_worker/bin/cryosparcw",' /central/groups/$GROUPDIR/$USER/software/cryosparc/cluster_info.json
+sed -i '4i\ \ \ \ "cache_path" : "/central/scratchio/'"$USER"'",' /central/groups/$GROUPDIR/$USER/software/cryosparc/cluster_info.json
 }
 
 
